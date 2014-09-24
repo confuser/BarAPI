41c3f3a248970dfa83b92a67965a5dd320201aa8
James Mortemore <jamesmortemore@gmail.com>
2014-09-24 15:43:57 +0100

    Fix for latest spigot patch

--- src/me/confuser/barapi/nms/v1_8Fake.java
+++ src/me/confuser/barapi/nms/v1_8Fake.java
@@ -135,7 +135,7 @@ public class v1_8Fake extends FakeDragon {
 		Object packet = null;
 
 		try {
-			packet = PacketPlayOutEntityTeleport.getConstructor(new Class<?>[] { int.class, int.class, int.class, int.class, byte.class, byte.class, boolean.class }).newInstance(this.id, loc.getBlockX() * 32, loc.getBlockY() * 32, loc.getBlockZ() * 32, (byte) ((int) loc.getYaw() * 256 / 360), (byte) ((int) loc.getPitch() * 256 / 360), false);
+			packet = PacketPlayOutEntityTeleport.getConstructor(new Class<?>[] { int.class, int.class, int.class, int.class, byte.class, byte.class, boolean.class, boolean.class }).newInstance(this.id, loc.getBlockX() * 32, loc.getBlockY() * 32, loc.getBlockZ() * 32, (byte) ((int) loc.getYaw() * 256 / 360), (byte) ((int) loc.getPitch() * 256 / 360), false, false);
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		} catch (SecurityException e) {
